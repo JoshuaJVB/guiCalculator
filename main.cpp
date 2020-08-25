2a19d9da089a564b4ddc872672ae2ac063f7829c
Joshua Van Baalen <joshuavanbaalen@gmail.com>
2020-08-25 16:11:03 -0700

    added divide by 0 handling

--- main.cpp
+++ main.cpp
@@ -13,11 +13,17 @@ int main() {
 	calculator c;
 
 	cout << "Calculator Console Application" << endl;
-	cout << "Please enter operation: " << endl;
+	cout << "Please enter operation: "       << endl;
 
 	while (true) {
 		cin >> x >> oper >> y;
-		result = c.Calculate(x, oper, y);
+		if ((y == 0) && (oper == '/')) {
+			cout << "Division by 0 exception" << endl;
+			continue;
+		}
+		else {
+			result = c.Calculate(x, oper, y);
+		}
 		cout << "Result is: " << result << endl;
 	}
 
